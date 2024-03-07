facb20138a0a41bf79ae223c62e0c6d95fdcb57d
GuDahoon <iamkdh1@naver.com>
2024-03-07 15:30:43 +0900

    studying fastapi and svelte

---
+++ fast_svelte_practice/projects/myapi/frontend/src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  let message;
+
+  fetch("http://localhost:8000/api/question/list").then((response) => {
+    response.json().then((json) => {
+      message = json.message;
+    })
+  })
+</script>
+
+<h1>{message}</h1>
